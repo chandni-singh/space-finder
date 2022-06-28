40c8e1bfb8c564b493670b81594b3f21e988a4cc
Chandni <chandni1141996@gmail.com>
2022-06-28 19:49:04 +0530

    Implemeted Login component and fixed TS src folder

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { User } from "../model/Model";
 import { AuthService } from "../services/AuthService";
+import Login from "./Login";
 
 interface AppState {
   user: User | undefined;
@@ -10,7 +11,12 @@ class App extends React.Component<{}, AppState> {
   private authService: AuthService = new AuthService();
 
   render() {
-    return <div>App works with class Component</div>;
+    return (
+      <div>
+        App works with class Component
+        <Login authService={this.authService} />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Login.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { AuthService } from "../services/AuthService";
+
+interface LoginProps {
+  authService: AuthService;
+}
+
+interface LoginState {
+  userName: string;
+  password: string;
+  loginAttempted: boolean;
+  loginSuccessful: boolean;
+}
+
+class Login extends React.Component<LoginProps, LoginState> {
+  state: LoginState = {
+    userName: "",
+    password: "",
+    loginAttempted: false,
+    loginSuccessful: false,
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Please login</h2>
+        <form>
+          <input value={this.state.userName} />
+          <br />
+          <input value={this.state.password} type="password" />
+          <br />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
