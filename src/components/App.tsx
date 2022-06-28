0ecfe6917112f54684bf3c795358946b5beb38a3
Chandni <chandni1141996@gmail.com>
2022-06-28 19:34:55 +0530

    Added user interface and implemented auth service using class component in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { User } from "../model/Model";
+import { AuthService } from "../services/AuthService";
 
-function App() {
-  return <div>App works</div>;
+interface AppState {
+  user: User | undefined;
+}
+
+class App extends React.Component<{}, AppState> {
+  private authService: AuthService = new AuthService();
+
+  render() {
+    return <div>App works with class Component</div>;
+  }
 }
 
 export default App;
